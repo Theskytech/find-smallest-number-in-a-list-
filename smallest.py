3729e6661352d1eaa4c190fbdeece6cf4e75cea8
Akash Kumar <126797717+Theskytech@users.noreply.github.com>
2023-03-03 21:12:51 +0530

    Add files via upload

---
+++ smallest.py
@@ -0,0 +1,9 @@
+# python program to find the smallest number
+letsupgrade = [5,7,9,14,10,20,4]
+letsupgrade.sort()
+
+# smallest number
+small = letsupgrade[0]
+
+# print the smallest number
+print("Smallest number in the list is: ",small)
